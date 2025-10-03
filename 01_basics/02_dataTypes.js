c2a0122529cbcff6f3aa2aa8b5893b279386e8ec
infosecankitsingh <infosecankitsingh@gmail.com>
2025-10-03 08:15:59 +0000

    Datatypes intorduction

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict" // treat all JS code as newer version > use newer classes and modules
+
+// alert("hello") // this will giver error as it is being run in node.js
+//Javascript engine is hidden inside browser, so don't directly use in code
+
+//we are using nodejs,not browser
+
+console.log(3+3); console.log("Ankit") // no code readability
+
+console.log(3
+    +
+    3) // code readability should be high
+
+//documentation is mdn from mozilla
+//original documentation - https://tc39.es/ecma262/
+
+
+let name = "Ankit"
+let age = 32 //number
+let isLoggedIn = false // boolean
+
+//number => 2 to power 53
+//bigint > if no is too big to be consumed like stock data
+//string => ""
+// boolean => true/false
+// null => standalone value representation of empty value
+// undefined => variable is declared but value is not assigned
+// symbol => for uniqueness ,,, will use in react
+
+//object 
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+//typeof null is "object"
+
+//typeof undefined is "undefined"
